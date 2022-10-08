4ba52859793a965033b4ca1cc212d09c6703c7e1
TZGyn <65798228+TZGyn@users.noreply.github.com>
2022-10-08 20:40:21 +0800

    added keyoverlay

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ let pp_current = document.getElementById('pp_current');
 let pp_separator = document.getElementById('pp_separator');
 let pp_max = document.getElementById('pp_max');
 let pp_suffix = document.getElementById('pp_suffix');
+let h100 = document.getElementById('h100_count');
+let h50 = document.getElementById('h50_count');
+let h0 = document.getElementById('h0_count');
+let sb = document.getElementById('sb_count');
 
 // Hit counter
 let hit_counter_container = document.getElementById('hit_counter_container');
@@ -22,8 +26,19 @@ let hit_counter_container = document.getElementById('hit_counter_container');
 let menu_counter = document.getElementById('menu_counter');
 let menu_pp_max = document.getElementById('menu_pp_max');
 
+// Key overlay
+let keys = document.getElementById('keys');
+
 // Variables
-let tempCombo, tempPP;
+let tempCombo,
+	tempPP,
+	tempMaxPP,
+	tempH100,
+	tempH50,
+	tempH0,
+	tempSB,
+	tempKeys,
+	tempMenuPP;
 let combo_container_width, pp_container_width;
 
 socket.onopen = () => {
@@ -95,82 +110,124 @@ let animation = {
 		decimal: '.',
 		suffix: '',
 	}),
+	k1: new KeyOverlay('k1', 'k1Tiles', {
+		speed: 0.4,
+		keyTextId: 'k1Text',
+	}),
+	k2: new KeyOverlay('k2', 'k2Tiles', {
+		speed: 0.4,
+	}),
+	m1: new KeyOverlay('m1', 'm1Tiles', {
+		speed: 0.4,
+	}),
+	m2: new KeyOverlay('m2', 'm2Tiles', {
+		speed: 0.4,
+	}),
 };
 
 socket.onmessage = (event) => {
 	let data = JSON.parse(event.data),
 		menu = data.menu,
 		gameplay = data.gameplay;
-	switch (menu.state) {
-		case 0:
-			bottom.style.opacity = 0;
-			hit_counter_container.style.opacity = 0;
-			menu_counter.style.opacity = 0;
-			break;
-		case 2:
-			menu_counter.style.opacity = 0;
-			bottom.style.opacity = 1;
-			hit_counter_container.style.opacity = 1;
-			// update scores
-			animation.pp_max.update(gameplay.pp.fc);
-			animation.h100.update(gameplay.hits[100]);
-			animation.h50.update(gameplay.hits[50]);
-			animation.h0.update(gameplay.hits[0]);
-			animation.sb.update(gameplay.hits.sliderBreaks);
-			// update scores (odometer)
-			if (tempCombo !== gameplay.combo.current) {
-				tempCombo = gameplay.combo.current;
-				combo_current.innerHTML = tempCombo;
-				animation.combo_current.update(combo_current.innerHTML);
-			}
-			if (tempPP !== gameplay.pp.current) {
-				tempPP = gameplay.pp.current;
-				pp_current.innerHTML = tempPP;
-				animation.pp_current.update(pp_current.innerHTML);
-			}
-			let combo_length = 0;
-			combo_length += gameplay.combo.current.toString().length;
-			combo_length *= 20;
-			combo_length += 50;
-			combo_container.style.width = combo_length + 'px';
-			let pp_length = 0;
-			pp_length += gameplay.pp.current.toString().length;
-			// if combo break, show max pp
-			if (gameplay.hits.sliderBreaks > 0 || gameplay.hits[0] > 0) {
-				pp_length += gameplay.pp.fc.toString().length;
-				pp_separator.style.opacity = '1';
-				pp_separator.style.fontSize = '25px';
-				pp_max.style.opacity = '1';
-				pp_max.style.fontSize = '25px';
-				pp_suffix.style.marginLeft = '0px';
-			} else {
-				pp_separator.style.opacity = '0';
-				pp_separator.style.fontSize = '0px';
-				pp_max.style.opacity = '0';
-				pp_max.style.fontSize = '0px';
-				pp_suffix.style.marginLeft = '-10px';
-			}
-			pp_length *= 20;
-			pp_length += 65;
-			pp_container.style.width = pp_length + 'px';
-			break;
-		case 5:
-			bottom.style.opacity = 0;
-			hit_counter_container.style.opacity = 0;
-			menu_counter.style.opacity = 1;
-			animation.menu_pp_max.update(menu.pp['100']);
-			break;
-		case 7:
-			bottom.style.opacity = 0;
-			hit_counter_container.style.opacity = 0;
-			menu_counter.style.opacity = 1;
-			animation.menu_pp_max.update(gameplay.pp.current);
-			break;
-		default:
-			bottom.style.opacity = 0;
-			hit_counter_container.style.opacity = 0;
-			menu_counter.style.opacity = 1;
-			animation.menu_pp_max.update(menu.pp['100']);
-			break;
+	// switch (menu.state) {
+	if (menu.state == 0) {
+		bottom.style.opacity = 0;
+		hit_counter_container.style.opacity = 0;
+		menu_counter.style.opacity = 0;
+		keys.style.opacity = 0;
+	} else if (menu.state == 2) {
+		menu_counter.style.opacity = 0;
+		bottom.style.opacity = 1;
+		hit_counter_container.style.opacity = 1;
+		keys.style.opacity = 1;
+		let combo_length = 0;
+		combo_length += gameplay.combo.current.toString().length;
+		combo_length *= 20;
+		combo_length += 50;
+		combo_container.style.width = combo_length + 'px';
+		let pp_length = 0;
+		pp_length += gameplay.pp.current.toString().length;
+		// if combo break, show max pp
+		if (gameplay.hits.sliderBreaks > 0 || gameplay.hits[0] > 0) {
+			pp_length += gameplay.pp.fc.toString().length;
+			pp_separator.style.opacity = '1';
+			pp_separator.style.fontSize = '25px';
+			pp_max.style.opacity = '1';
+			pp_max.style.fontSize = '25px';
+			pp_suffix.style.marginLeft = '0px';
+		} else {
+			pp_separator.style.opacity = '0';
+			pp_separator.style.fontSize = '0px';
+			pp_max.style.opacity = '0';
+			pp_max.style.fontSize = '0px';
+			pp_suffix.style.marginLeft = '-10px';
+		}
+		pp_length *= 20;
+		pp_length += 65;
+		pp_container.style.width = pp_length + 'px';
+	} else if (menu.state == 5) {
+		bottom.style.opacity = 0;
+		hit_counter_container.style.opacity = 0;
+		menu_counter.style.opacity = 1;
+		keys.style.opacity = 0;
+	} else if (menu.state == 7) {
+		bottom.style.opacity = 0;
+		hit_counter_container.style.opacity = 0;
+		menu_counter.style.opacity = 1;
+		keys.style.opacity = 0;
+		// animation.menu_pp_max.update(gameplay.pp.current);
+	} else {
+		bottom.style.opacity = 0;
+		hit_counter_container.style.opacity = 0;
+		menu_counter.style.opacity = 1;
+		keys.style.opacity = 0;
 	}
+	// update scores
+	if (tempMaxPP !== gameplay.pp.fc) {
+		tempMaxPP = gameplay.pp.fc;
+		pp_max.innerHTML = tempMaxPP;
+		animation.pp_max.update(pp_max.innerHTML);
+	}
+	if (tempH100 !== gameplay.hits[100]) {
+		tempH100 = gameplay.hits[100];
+		h100.innerHTML = tempH100;
+		animation.h100.update(h100.innerHTML);
+	}
+	if (tempH50 !== gameplay.hits[50]) {
+		tempH50 = gameplay.hits[50];
+		h50.innerHTML = tempH50;
+		animation.h50.update(h50.innerHTML);
+	}
+	if (tempH0 !== gameplay.hits[0]) {
+		tempH0 = gameplay.hits[0];
+		h0.innerHTML = tempH0;
+		animation.h0.update(h0.innerHTML);
+	}
+	if (tempSB !== gameplay.hits.sliderBreaks) {
+		tempSB = gameplay.hits.sliderBreaks;
+		sb.innerHTML = tempSB;
+		animation.sb.update(sb.innerHTML);
+	}
+	// update scores (odometer)
+	if (tempCombo !== gameplay.combo.current) {
+		tempCombo = gameplay.combo.current;
+		combo_current.innerHTML = tempCombo;
+		animation.combo_current.update(combo_current.innerHTML);
+	}
+	if (tempPP !== gameplay.pp.current) {
+		tempPP = gameplay.pp.current;
+		pp_current.innerHTML = tempPP;
+		animation.pp_current.update(pp_current.innerHTML);
+	}
+	// update menu pp preview
+	if (tempMenuPP !== gameplay.pp.current) {
+		tempMenuPP = menu.pp['100'];
+		menu_pp_max.innerHTML = tempMenuPP;
+		animation.menu_pp_max.update(menu_pp_max.innerHTML);
+	}
+	// update keys (on different thread)
+	animation.k1.update(gameplay.keyOverlay.k1);
+	animation.k2.update(gameplay.keyOverlay.k2);
+	animation.m1.update(gameplay.keyOverlay.m1);
+	animation.m2.update(gameplay.keyOverlay.m2);
 };
